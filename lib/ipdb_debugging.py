9cfba1188125c54ef3a6079cbfa0663bf2805fe8
Sadam Salat <sadamsalat@Sadams-MacBook-Air.local>
2025-06-15 18:16:51 -0500

    done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,8 @@
 import ipdb
 
 def plus_two(num):
+    ipdb.set_trace()
     num + 2
+
     return num
+print(plus_two(3))
--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -5,6 +5,7 @@ import ipdb
 def tracing_the_function():
     inside_the_function = "We're inside the function"
     print(inside_the_function)
+    
     print("We're about to stop because of ipdb!")
     ipdb.set_trace()
     this_variable_hasnt_been_interpreted_yet = \
